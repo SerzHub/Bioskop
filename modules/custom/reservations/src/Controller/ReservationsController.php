dd0454194a6bc9d1af8c6b92dea96801ccdcad8b
SerzHub <58301210+SerzHub@users.noreply.github.com>
2022-04-01 12:40:31 +0200

    Initial commit, with bugs (#2)
    * Initial commit, with bugs
    
    * 17 - Fixed bugs with routing
    
    * 17 - Added only necessary files
    
    * 17 - Fixed names of function, path to site, and deleted unnecessary comments

---
+++ modules/custom/reservations/src/Controller/ReservationsController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\reservations\Controller\ReservationsController.
+ */
+
+namespace Drupal\reservations\Controller;
+
+class ReservationsController {
+  public function return_all_movies() {
+    $movies = \Drupal::entityTypeManager() -> getStorage('node') -> loadByProperties(['type' => 'Movie']); 
+
+    return array(
+      '#theme' => 'reservations', 
+      '#movies' => $movies,
+      'title' => 'blssbbs',
+    );
+  }
+}
+
