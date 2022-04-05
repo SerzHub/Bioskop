513c18dd49bb70cefcbe1040104fa81fa32af7b7
SerzHub <sergej357.i@gmail.com>
2022-04-05 13:17:15 +0200

    #18 - Added letter s to success in reservation.js, and changed version of jquery in twig

--- modules/custom/reservations/js/reservations.js
+++ modules/custom/reservations/js/reservations.js
@@ -1,12 +1,14 @@
 function choosingGenre() {
     $("#genre").on("change", function () {
         $.ajax({
-            method: "GET", url: "./movie_reservations?type=" + $("#genre").val(),
-            succes: function () {
+            method: "GET",
+            url: "./movie_reservations?type=" + $("#genre").val(),
+            success: function () {
+                console.log('abc');
                 window.location.href = "./movie_reservations?type=" + $("#genre").val();
             }
-        })
-    })
+        });
+    });
 }
 
 choosingGenre();
