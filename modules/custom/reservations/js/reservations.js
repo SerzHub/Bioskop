a8be067dabd7dabd7d407dca4b921886a6712218
SerzHub <58301210+SerzHub@users.noreply.github.com>
2022-04-12 10:44:51 +0200

    #27-Added button for checking if name is written correctly (#7)
    * #27-Added button for checking if name is written correctly
    
    * #27 - Moved css code from js file into appropriate css file
    
    * #27 - Fixed bug with reloading page.
    
    * #27 - Changed name of form and class, and deleted unnecessary whitespaces

--- modules/custom/reservations/js/reservations.js
+++ modules/custom/reservations/js/reservations.js
@@ -13,13 +13,32 @@ function choosingGenre() {
 
 function SelectingMovie() {
     $(".item").click(function () {
-        if (!$(this).is('.itemChange')) {
-            $(this).toggleClass('itemChange')
+        if (!$(this).is('.selectedMovie')) {
+            $(this).toggleClass('selectedMovie')
             $(".button", this).show();
         } else {
-            $(".item").removeClass("itemChange")
+            $(".item").removeClass('selectedMovie')
             $(".button", this).hide();
         }
+    })
+}
+
+function validateInput() {
+
+
+    $("#formName").submit(function () {
+        const name = $('#customerName').val();
+        const name_without_numbers = /^[A-Z][a-z]+$/;
+
+        if (!name_without_numbers.test(name)) {
+
+            $(this).toggleClass('button1')
+            $(".button1", this).show();
+        } else {
+            $(".item").removeClass('button1')
+            $(".button1", this).hide();
+        }
+        return false;
     })
 }
 
@@ -27,5 +46,4 @@ choosingGenre();
 
 SelectingMovie();
 
-
-
+validateInput();
