cf2ab46515aa177b63a383e2bfa22b5b2c2cbeec
SerzHub <sergej357.i@gmail.com>
2022-04-12 10:19:14 +0200

    #27 - Changed name of form and class, and deleted unnecessary whitespaces

--- modules/custom/reservations/js/reservations.js
+++ modules/custom/reservations/js/reservations.js
@@ -13,11 +13,11 @@ function choosingGenre() {
 
 function SelectingMovie() {
     $(".item").click(function () {
-        if (!$(this).is('.itemChange')) {
-            $(this).toggleClass('itemChange')
+        if (!$(this).is('.selectedMovie')) {
+            $(this).toggleClass('selectedMovie')
             $(".button", this).show();
         } else {
-            $(".item").removeClass('itemChange')
+            $(".item").removeClass('selectedMovie')
             $(".button", this).hide();
         }
     })
